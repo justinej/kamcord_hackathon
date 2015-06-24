809025516b271b7c387081f0e7283dd1206a1d30
Justine <justinej@mit.edu>
2015-06-24 12:05:40 -0700

    made a SurfaceView class

---
+++ app/src/main/java/com/example/justine/gamekamcordhackathon/GamePanel.java
@@ -0,0 +1,37 @@
+package com.example.justine.gamekamcordhackathon;
+
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.content.Context;
+import android.view.MotionEvent;
+
+/**
+ * Created by justine on 6/24/15.
+ */
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+
+    public GamePanel(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {}
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
+    }
+
+
+}
