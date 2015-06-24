a9266a04904cd29c867571aa47df5ad3d12a2bd1
Justine <justinej@mit.edu>
2015-06-24 13:17:20 -0700

    added loop stuff, not sure if useful

--- app/src/main/java/com/example/justine/gamekamcordhackathon/GamePanel.java
+++ app/src/main/java/com/example/justine/gamekamcordhackathon/GamePanel.java
@@ -9,11 +9,15 @@ import android.view.MotionEvent;
  * Created by justine on 6/24/15.
  */
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+    private MainThread thread;
 
     public GamePanel(Context context) {
         super(context);
 
         getHolder().addCallback(this);
+        thread = new MainThread(getHolder(), this);
+        // make gamePanel focusable true so it can handle events?
+        setFocusable(true);
     }
 
     @Override
@@ -21,11 +25,21 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
     }
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {}
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch(InterruptedException e) {e.printStackTrace();}
+            retry = false;
+        }
+    }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        thread.setRunning(true);
+        thread.start();
     }
 
     @Override
@@ -33,5 +47,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
         return super.onTouchEvent(event);
     }
 
+    public void update() {
+
+    }
 
 }
---
+++ app/src/main/java/com/example/justine/gamekamcordhackathon/MainThread.java
@@ -0,0 +1,51 @@
+package com.example.justine.gamekamcordhackathon;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by justine on 6/24/15.
+ */
+public class MainThread extends Thread {
+    private long time;
+
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+    public static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void run(){
+        try {
+            while(running)
+                time = System.nanoTime();
+                canvas = null;
+                System.out.println(time);
+
+            canvas = this.surfaceHolder.lockCanvas();
+            synchronized (surfaceHolder) {
+                this.gamePanel.update();
+                this.gamePanel.draw(canvas);
+            }
+        } catch(Exception e){}
+        finally {
+            if (canvas != null) {
+                try {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+                catch (Exception e) {e.printStackTrace();}
+            }
+        }
+
+    }
+
+    public void setRunning(boolean b){
+        running = b;
+    }
+}
